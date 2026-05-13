aef72976376042b7472e6963b9ed89dee061f7a4
Yichm <0Luke.Luke0@gmail.com>
2026-05-14 00:51:48 +0800

    Add lexer and parser. Handle redirect .

--- main.c
+++ main.c
@@ -1,27 +1,26 @@
+#include <asm-generic/errno-base.h>
+#include <ctype.h>
+#include <errno.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
 #define true 1
 #define false 0
 
 enum PipechainConnector {
-    CONN_ANY, CONN_FAILURE, CONN_SUCCESS
-};
-
-struct Redirect {
-    char *path;
-    int fd;
-    int flags;
+    CONN_FAILURE, CONN_SUCCESS
 };
 
 struct SimpleCommand {
+    int oredir_append;
     char **argv;
-    struct Redirect *redirect;
-    size_t nredir;
+    char *iredir_path;
+    char *oredir_path;
     struct SimpleCommand *next;
 };
 
@@ -46,7 +45,8 @@ int (*builtin_func[]) (char **) = {
 };
 int builtin_count = sizeof(builtin_func) / sizeof(builtin_func[0]);
 
-int builtin_cd(char **args) {
+int builtin_cd(char **args)
+{
     if (args[0] == NULL) {
         char *msg = "builtin: expect argument to \"cd\"";
         write(STDERR_FILENO, msg, strlen(msg));
@@ -59,11 +59,13 @@ int builtin_cd(char **args) {
     return 0;
 }
 
-int builtin_exit(char **args) {
+int builtin_exit(char **args)
+{
     return -1;
 }
 
-int movfd(int old, int new) {
+int movfd(int old, int new)
+{
     if (old == new) {
         return new;
     }
@@ -76,8 +78,29 @@ int movfd(int old, int new) {
     return new;
 }
 
-void exec_simple_command(struct SimpleCommand *cmd) {
-    
+void setup_redirect(char *ipath, char *opath, int *ifd, int *ofd)
+{
+    if (ifd != NULL) {
+        *ifd = -1;
+        if (ipath != NULL) {
+            *ifd = open(ipath, O_RDONLY);
+            if (*ifd >= 0)
+                dup2(*ifd, STDIN_FILENO);
+        }
+    }
+    if (ofd != NULL) {
+        int append = *ofd;
+        *ofd = -1;
+        if (opath != NULL) {
+            if (append) {
+                *ofd = open(opath, O_WRONLY|O_CREAT|O_APPEND);
+            } else {
+                *ofd = open(opath, O_WRONLY|O_CREAT|O_TRUNC);
+            }
+            if (*ofd >= 0)
+                dup2(*ofd, STDOUT_FILENO);
+        }
+    }
 }
 
 int execute_pipechain(struct Pipechain *chain) {
@@ -88,7 +111,21 @@ int execute_pipechain(struct Pipechain *chain) {
         // this is important for cd and exit.
         for (int i = 0; i < builtin_count; i++) {
             if (strcmp(current->argv[0], builtin_str[i]) == 0) {
-                return builtin_func[i](&(current->argv[1]));
+                int original_stdin = dup(STDIN_FILENO);
+                int original_stdout = dup(STDOUT_FILENO);
+                int ifd, ofd;
+                ofd = current->oredir_append;
+                setup_redirect(current->iredir_path, current->oredir_path, &ifd, &ofd);
+                int status = builtin_func[i](&(current->argv[1]));
+                dup2(original_stdin, STDIN_FILENO);
+                dup2(original_stdout, STDOUT_FILENO);
+                if (ifd >= 0) 
+                    close(ifd);
+                if (ofd >= 0)
+                    close(ofd);
+                close(original_stdin);
+                close(original_stdout);
+                return status;
             }
         }
     }
@@ -120,6 +157,8 @@ int execute_pipechain(struct Pipechain *chain) {
                 }
             }
 
+            int ofd = current->oredir_append;
+            setup_redirect(current->iredir_path, current->oredir_path, NULL, &ofd);
             // builtins
             for (int i = 0; i < builtin_count; i++) {
                 if (strcmp(current->argv[0], builtin_str[i]) == 0) {
@@ -132,6 +171,14 @@ int execute_pipechain(struct Pipechain *chain) {
             }
 
             execvp(current->argv[0], current->argv);
+            perror("execvp");
+            if (errno == ENOENT) {
+                exit(127);
+            }
+            if (errno == EACCES) {
+                exit(126);
+            }
+            exit(1);
         } else {  /* Parent. Keep processing in the pipechain */
             if (inputfd != STDIN_FILENO) {
                 if (close(inputfd) == -1) {
@@ -174,10 +221,11 @@ int execute_pipechain(struct Pipechain *chain) {
     return status;
 }
 
-int execute_command(struct Pipechain *cmd) {
+int execute_command(struct Pipechain *cmd)
+{
     int status = 0;
     while (cmd != NULL) {
-        status = execute_command(cmd);
+        status = execute_pipechain(cmd);
         
         if (status == 0 && cmd->connector == CONN_FAILURE
             || status != 0 && cmd->connector == CONN_SUCCESS) {
@@ -188,27 +236,180 @@ int execute_command(struct Pipechain *cmd) {
     return status;
 }
 
-/*
-int extend_compound_command(struct CompoundCommand *cmd, char *line)
-{
-    int inside_squote = 0;
-    int inside_dquote = 0;
-    int inside_file = 0;
+// Helper to skip whitespace
+const char* skip_ws(const char *s) {
+    while (*s && isspace(*s)) 
+        s++;
+    return s;
+}
 
-    if (cmd->pipechain_count < 1) {
-        struct Pipechain *newpipechain = malloc(sizeof(struct Pipechain));
-        if (newpipechain == NULL) {
-            perror("malloc");
-            exit(EXIT_FAILURE);
+// Extract next token without modifying original string
+char* get_token(const char **s) {
+    *s = skip_ws(*s);
+    if (**s == '\0')
+        return NULL;
+
+    const char* start = *s;
+    int only_standard_words = false;
+    if (**s == '\'') {
+        (*s)++;
+        while (**s && **s != '\'') {
+            (*s)++;
         }
-        newpipechain->first = NULL;
-        newpipechain->reverse_status = false;
-        newpipechain->final = false;
-        cmd->pipechain = newpipechain;
-        cmd->pipechain_count = 1;
+        if (**s) {
+            (*s)++;
+        }
+        only_standard_words = true;
+        // POSIX says that we shouldn't delimit tokens at end of quote.
+    }
+    // Handle metacharacters as single tokens
+    if (!only_standard_words
+        && (strncmp(*s, "&&", 2) == 0
+            || strncmp(*s, "||", 2) == 0
+            || strncmp(*s, ">>", 2) == 0)) {
+        *s += 2;
+    } else if (!only_standard_words && strchr("|<>", **s)) {
+        (*s)++;
+    } else {
+        // Standard word
+        // TODO handle double quotes
+        while (**s && !isspace(**s) && !strchr("|<>!&", **s)) {
+            // backslash escape
+            if (**s == '\\' && *((*s)+1)) {
+                (*s)++;
+            }
+            (*s)++;
+        }
+    }
+    size_t len = *s - start;
+    if (len == 0)
+        return NULL;
+    char *token = malloc(len + 1);
+    memcpy(token, start, len);
+    token[len] = '\0';
+    return token;
+}
+
+void remove_squote(char *s)
+{
+    int outside_quote = true;
+    while (*s) {
+        if (outside_quote && *s == '\\' && *(s + 1)) {
+            s += 2;
+            continue;
+        }
+        if (*s == '\'') {
+            outside_quote = !outside_quote;
+            char *c = s;
+            while (*c) {
+                *c = *(c + 1);
+                c++;
+            }
+        }
+        s++;
     }
 }
-*/
+
+struct Pipechain *parse_line(const char *line)
+{
+    const char *cursor = line;
+    struct Pipechain *head = NULL;
+    struct Pipechain *previous_pipe = NULL;
+
+    while (true) {
+        cursor = skip_ws(cursor);
+        if (*cursor == '\0') {
+            break;
+        }
+        struct Pipechain *current_pipe = calloc(1,sizeof(struct Pipechain));
+        if (head == NULL)
+            head = current_pipe;
+        if (previous_pipe != NULL) {
+            previous_pipe->next = current_pipe;
+        }
+    
+        current_pipe->reverse_status = false;
+        if (*cursor == '!') {
+            current_pipe->reverse_status = true;
+            cursor++;
+        }
+
+        struct SimpleCommand *previous_cmd = NULL;
+        int pipeline_done = false;
+        while (!pipeline_done) {
+            struct SimpleCommand *current_cmd = calloc(1, sizeof(struct SimpleCommand));
+            if (current_pipe->simple_commands == NULL)
+                current_pipe->simple_commands = current_cmd;
+            if (previous_cmd != NULL)
+                previous_cmd->next = current_cmd;
+            previous_cmd = current_cmd;
+
+            char *token;
+            size_t argv_capacity = 8;
+            int argc = 0;
+            current_cmd->argv = calloc(argv_capacity, sizeof(char *));
+            while (1) {
+                token = get_token(&cursor);
+                if (token == NULL) {
+                    pipeline_done = 1;
+                    break;
+                }
+                if (strcmp("|", token) == 0) {
+                    free(token);
+                    break;
+                }
+                if (strcmp("&&", token) == 0) {
+                    free(token);
+                    current_pipe->connector = CONN_SUCCESS;
+                    pipeline_done = 1;
+                    break;
+                }
+                if (strcmp("||", token) == 0) {
+                    free(token);
+                    current_pipe->connector = CONN_FAILURE;
+                    pipeline_done = 1;
+                    break;
+                }
+                if (strcmp(">>", token) == 0
+                    || strchr("<>", *token)) {
+                    char *fname = get_token(&cursor);
+                    remove_squote(fname);
+                    if (strlen(fname) == 0) {
+                        free(token);
+                        free(fname);
+                        pipeline_done = 1;
+                        break;
+                    }
+                    if (strchr(token, '>') != NULL) {
+                        free(current_cmd->oredir_path);
+                        current_cmd->oredir_path = fname;
+                        if (strcmp(">>", token) == 0) {
+                            current_cmd->oredir_append = true;
+                        } else {
+                            current_cmd->oredir_append = false;
+                        }
+                    }
+                    continue;
+                }
+                if (argc > argv_capacity - 1) {
+                    argv_capacity *= 2;
+                    current_cmd->argv = reallocarray(current_cmd->argv, argv_capacity, sizeof(char *));
+                    if (current_cmd->argv == NULL) {
+                        perror("calloc");
+                        exit(1);
+                    }
+                    remove_squote(token);
+                    current_cmd->argv[argc] = token;
+                    argc++;
+                }
+            }
+
+            current_cmd->argv[argc] = NULL;
+        }
+    }
+
+    return head;
+}
 
 /**
  * @brief A reimplementation of the getline function using low-level IO.
@@ -253,5 +454,5 @@ ssize_t get_line(char **buf, size_t *bufsize, int fd)
 
 int main(int argc, char *argv[], char *envp[])
 {
-
+    parse_line("echo 'Hello, world!' | cat && echo 'done'");
 }
